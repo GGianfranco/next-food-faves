f0afdbd90d85edef9ebdcda3b79fbfce080c50ba
GGianfranco <freelancegianfranco@gmail.com>
2022-03-03 15:49:12 +0800

    added onClick and onFirstLoad FoodCard animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { ThemeProvider } from "next-themes";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (window.sessionStorage.getItem("firstLoadAnimated") === null) {
+      window.sessionStorage.setItem("firstLoadAnimated", "1");
+    }
+  }, []);
   return (
     <ThemeProvider>
       <Layout>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,48 +7,42 @@ import styles from "../styles/Home.module.css";
 const foodFaves = [
   {
     name: "Coleslaw",
-    image:
-      "https://www.chowhound.com/a/img/resize/62679fa3d1cceba5c0da0c667b3af6a621532df6/2018/06/best-coleslaw-recipe-chowhound.jpg?fit=bounds&height=400&width=400",
+    image: "/coleslaw.webp",
     description:
       "The fact that it's a vegetable is already enough for me to like to eat it but the combination of well cut strips of cabbage and carrots with a little bit of lemon and oozing mayonnaise makes me like to eat it more!",
     rating: 5,
   },
   {
     name: "Burnt Sunny Side Up!",
-    image:
-      "https://static.onecms.io/wp-content/uploads/sites/19/2018/02/13/sunny-side-up-eggs-hero-2000.jpg",
+    image: "/sunny-side-up.jpg",
     description:
       "I like sunny side up especially when it is slightly burnt. The crunchiness of the burnt area might sound but it significantly adds a pleasant texture to the mouth",
     rating: 5,
   },
   {
     name: "Buttery Scrambled Egg",
-    image:
-      "https://www.thespruceeats.com/thmb/qom1mjUXWVYVdyv3mwnXeIQO-Po=/4444x3333/smart/filters:no_upscale()/scrambled-eggs-58a701ac5f9b58a3c91cbebd.jpg",
+    image: "/scrambled.jpg",
     description:
       "Buttery Scrambled Egg gives the opposite feeling of what the Burnt Sunny Side Up! offers. The creamy texture from the scrambled egg is perfect to partner with a toasted garlic bread.",
     rating: 4,
   },
   {
     name: "Shanghai",
-    image:
-      "https://www.kawalingpinoy.com/wp-content/uploads/2017/06/lumpiang-shanghai-4.jpg",
+    image: "/shanghai.webp",
     description:
       "One of the three legends in children parties (with Hotdog and Spaghetti). Shanghai I would say is the best.",
     rating: 5,
   },
   {
     name: "Vinegar",
-    image:
-      "https://www.thespruceeats.com/thmb/sxilu_dZIoHYwLOLzu1A_x66Zdg=/1495x1121/smart/filters:no_upscale()/Vinegar-twenty20_117eafb1-ad35-44e3-bfda-70f0572bd144-590752e83df78c5456a39f4a.jpg",
+    image: "/vinegar.jpg",
     description:
       'The "good taste" it is known for I don\'t think overweighs the "not so good smell" it comes with. So I\'ll just give it a one for existing.',
     rating: 1,
   },
   {
     name: "Angel's Burger",
-    image:
-      "https://4.bp.blogspot.com/-pqJtCAvHJgw/WsEMT4OenII/AAAAAAAAaKw/Kz1lkutru90_T-b9tDta13LitM2CL0dGQCLcBGAs/s1600/Snapseed_1.jpg",
+    image: "/burger.jpg",
     description: "My study buddy in my college days. I miss you.",
     rating: 3,
   },
