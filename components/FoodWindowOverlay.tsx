fd975a66f06ae32b6fad5e703ae6ecfcaa08eef5
GGianfranco <freelancegianfranco@gmail.com>
2022-03-04 11:47:54 +0800

    fixed show props

--- components/FoodWindowOverlay.tsx
+++ components/FoodWindowOverlay.tsx
@@ -7,36 +7,33 @@ type FoodWindowOverlayProps = {
   image: string;
   description: string;
   stringRating: string;
-  show: boolean;
-  setShow: (show: boolean) => void;
+  setShow: (any: boolean) => void;
 };
 
 const FoodWindowOverlay: NextPage<FoodWindowOverlayProps> = (props) => {
-  const { name, image, description, stringRating, show, setShow } = props;
+  const { name, image, description, stringRating, setShow } = props;
 
   return (
-    show && (
-      <>
-        <div className={styles.container} onClick={() => setShow(false)}>
-          <div className={styles.modal}>
-            <div className={styles.image}>
-              <Image
-                src={image}
-                alt={image}
-                width={500}
-                height={500}
-                layout="responsive"
-              />
-            </div>
-            <div className={styles.info}>
-              <div className={styles.name}>{name}</div>
-              <div className={styles.rating}>{stringRating}</div>
-              <div className={styles.description}>{description}</div>
-            </div>
+    <>
+      <div className={styles.container} onClick={() => setShow(false)}>
+        <div className={styles.modal}>
+          <div className={styles.image}>
+            <Image
+              src={image}
+              alt={image}
+              width={500}
+              height={500}
+              layout="responsive"
+            />
+          </div>
+          <div className={styles.info}>
+            <div className={styles.name}>{name}</div>
+            <div className={styles.rating}>{stringRating}</div>
+            <div className={styles.description}>{description}</div>
           </div>
         </div>
-      </>
-    )
+      </div>
+    </>
   );
 };
 
